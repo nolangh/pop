1904ecaad4b1d65f48232f637791f07c2aea8b51
nolangh <nolanghiggins@gmail.com>
2023-01-15 15:59:30 -0700

    -a

--- app/routes/index.jsx
+++ app/routes/index.jsx
@@ -1,5 +1,6 @@
 export default function Index() {
 	return (
+		/*This is a test for the repo connect */
 		<div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
 			<h1>Welcome to Remix</h1>
 			<ul>
