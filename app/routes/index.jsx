eb030fffecb7c0e926b5457c16e811da46c94324
nolangh <nolanghiggins@gmail.com>
2023-02-23 18:24:29 -0700

    -a

--- app/routes/index.jsx
+++
@@ -1,6 +0,0 @@
-export default function Index() {
-	return (
-		/*This is a test for the repo connect */
-		<div className="text-red-500">This is index.js</div>
-	);
-}
--- app/routes/signIn.jsx
+++
@@ -1,3 +0,0 @@
-export default function signIn() {
-	<div>This will be the Sign in page</div>;
-}
